4aa7e4e0babe8751924909a7eb6f4251c9007015
CsNoel <csenyinoel3@gmail.com>
2026-05-13 17:34:11 -0400

    Completed final project tasks

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -17,6 +17,17 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, headers=headers, json=input_json)
 
+    # 🔴 ERROR HANDLING (TASK 7)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     formatted_response = json.loads(response.text)
 
     emotions = formatted_response["emotionPredictions"][0]["emotion"]
